a5389113f06959be0af1498927e96a51da9ac982
martip23 <patrickmartinez77@gmail.com>
2016-11-03 11:01:14 -0500

    Preparing for refactoring of count

--- lib/numbers.asm
+++ lib/numbers.asm
@@ -51,8 +51,42 @@ n_dtoi_top:
 n_dtoi_done:
         ret
 
-n_n_btoi:
-        mov     rax, 42
+n_btoi: ; Convert from binary to int
+        xor     rax, rax    ; reset accumulator
+        xor     rcx, rcx    ; reset counter
+
+        ;**Check for 0b_ at start of rdi**
+        mov     dl, [rdi+rcx]
+        cmp     dl, '0'
+        jne     n_btoi_done
+        inc     rcx
+
+        mov     dl, [rdi+rcx]
+        cmp     dl, 'b'
+        jne     n_btoi_done
+        inc     rcx
+
+        mov     dl, [rdi+rcx]
+        cmp     dl, '_'
+        jne     n_btoi_done
+        inc     rcx
+        ;**End parsing, this effectively skips the indexes for '0b_'**
+
+n_btoi_count:
+        mov dl, [rdi+rcx]   ; Will start reading index after '0b_'
+        cmp dl, 0           ; Searching for end of string
+
+        jz  n_btoi_convert  ; Once at end of string, start conversion
+                            ; This effectively starts conversion from
+                            ; Rightmost bit
+        inc rcx
+        jmp n_btoi_count
+
+n_btoi_convert:
+        mov dl, [rdi+rcx]   ; At this point, rdi+rcs will be the
+                            ; LAST num in index
+        
+n_btoi_done:
         ret
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,25 @@ int main(int argc, char * argv[]) {
     cout << "Lab 3 - Number Conversion\n";
     cout << "\tAuthor: Patrick Martinez\n";
     cout << "\tDate  : Nov 1, 2016\n\n";
+    cout << "Binary entries should start with 0b_\n\n";
 
-    cout << "Enter a decimal number to convert: ";
+    cout << "Enter a decimal or binary number to convert: ";
     cin >> buffer;
-    cout << "You entered : " << buffer << endl;
-    ival = n_dtoi(buffer);
-    cout << "\tAfter converting, this gave: " << ival << endl;    
-    ival = n_len(buffer);
+
+    if (buffer[0] == '0' && buffer[1] == 'b' && buffer [2] == '_'){
+        cout << "Detected binary entry: " << buffer << endl;
+        ival = n_btoi(buffer);
+        cout << "\tAfter converting, this gave: " << ival << endl;
+    }
+
+    else {
+        cout << "You entered : " << buffer << endl;
+        ival = n_dtoi(buffer);
+        cout << "\tAfter converting, this gave: " << ival << endl;    
+   
+    }
+
+    ival = n_len(buffer); 
     cout << "\tThe input string was " << ival 
         << " characters long" << endl;
 
